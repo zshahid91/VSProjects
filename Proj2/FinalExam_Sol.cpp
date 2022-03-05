75c22c9222c5a4b554e17b1132db4176d3fe07dd
Zohaib Shahid <zshahid91@gmail.com>
2022-03-05 14:24:26 +0500

    Added a new file

---
+++ Proj2/FinalExam_Sol.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<cmath>
+#include<iomanip>
+
+using namespace std;
+
+//#define G 6.67e-11                          
+                                               
+int main(){                                    
+    float num = 32.5;                         
+    bool isStudyCool = true;
+    char grade = 'A'; // or string grade = "A"                 
+    string name = “roomba”;                        
+    cout << “My marks in the sociology exam = ” << num;
+    return 0;
+    }
