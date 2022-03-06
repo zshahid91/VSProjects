a8fba5299e5b28ad1b82f37ce907a6cc28ff2b0d
Zohaib Shahid <zshahid91@gmail.com>
2022-03-06 13:19:03 +0500

    Just made some changes to the exam solution for Mid term exams

--- Proj2/FinalExam_Sol.cpp
+++ Proj2/FinalExam_Sol.cpp
@@ -6,11 +6,18 @@ using namespace std;
 
 //#define G 6.67e-11                          
                                                
+#include<iostream>
+
+using namespace std;
+
 int main(){                                    
     float num = 32.5;                         
     bool isStudyCool = true;
     char grade = 'A'; // or string grade = "A"                 
     string name = “roomba”;                        
     cout << “My marks in the sociology exam = ” << num;
+    cout << "Name: " << name;
+    cout << "Grade = " << grade;
+
     return 0;
     }
