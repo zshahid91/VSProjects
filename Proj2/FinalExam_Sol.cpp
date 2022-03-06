cdb61fc1feaff64eb4255ac72fcb05ceb2d35e19
Zohaib Shahid <zshahid91@gmail.com>
2022-03-06 14:33:44 +0500

    Working on the solution of the quizzes

--- Proj2/FinalExam_Sol.cpp
+++ Proj2/FinalExam_Sol.cpp
@@ -7,17 +7,21 @@ using namespace std;
 //#define G 6.67e-11                          
                                                
 #include<iostream>
+#include<cmath>
 
 using namespace std;
 
+#define pi 3.14159
+#define g 9.81
+
 int main(){                                    
-    float num = 32.5;                         
-    bool isStudyCool = true;
-    char grade = 'A'; // or string grade = "A"                 
-    string name = “roomba”;                        
-    cout << “My marks in the sociology exam = ” << num;
-    cout << "Name: " << name;
-    cout << "Grade = " << grade;
+    float a = 0, b = 2, c = 3;
+    a = b++;
+    b = b--;
+    c = ++b;
+    b--;
+
+    cout << a << " " << b << " " << c << endl;
 
     return 0;
     }
