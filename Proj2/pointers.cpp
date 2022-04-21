1adf5e8a26fb6473abea503968f91f959287f3c3
Zohaib Shahid <zshahid91@gmail.com>
2022-04-21 15:21:30 +0500

    Pointers & by-reference explained further

--- Proj2/pointers.cpp
+++ Proj2/pointers.cpp
@@ -6,16 +6,18 @@ using namespace std;
 
 int main(){
     string food = "teenday";
+    int age = 22;
 
     cout << food << endl;
 
     cout << &food << endl;
 
     string* ptr = &food;
+    int *age_ptr = &age;
     // string * ptr = &food;
     // string *ptr = &food;
 
-    cout << ptr;
+    cout << ptr << "    " << age_ptr << "   " << ptr2;
 
     return 0;
 }
